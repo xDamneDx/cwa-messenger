f90ac7645eacdec23d36b21d3876c8f374252da6
xDamneDx <xdamnedx@gmail.com>
2023-05-18 18:36:23 +0200

    auth ui

---
+++ app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,20 @@
+import { IconType } from "react-icons";
+
+interface Props {
+  icon: IconType;
+  onClick: () => void;
+}
+
+const AuthSocialButton: React.FC<Props> = ({ icon: Icon, onClick }) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="inline-flex justify-center w-full px-4 py-2 text-gray-500 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
+    >
+      <Icon />
+    </button>
+  );
+};
+
+export default AuthSocialButton;
