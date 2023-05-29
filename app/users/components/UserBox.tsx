8b54db482f2cc768c430934eb6fabc5663ffdcdb
xDamneDx <xdamnedx@gmail.com>
2023-05-29 14:35:07 +0200

    add user list component

---
+++ app/users/components/UserBox.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Avatar from "@/app/components/Avatar";
+import { User } from "@prisma/client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+
+interface Props {
+  data: User;
+}
+
+const UserBox: React.FC<Props> = ({ data }) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = useCallback(() => {
+    setIsLoading(true);
+    axios
+      .post("/api/conversations", {
+        userId: data.id,
+      })
+      .then(({ data }) => {
+        router.push(`/conversations/${data.id}`);
+      })
+      .finally(() => setIsLoading(false));
+  }, [data, router]);
+
+  return (
+    <div
+      onClick={() => handleClick()}
+      className="relative flex items-center w-full p-3 space-x-3 transition bg-white rounded-lg cursor-pointer hover:bg-neutral-100"
+    >
+      <Avatar user={data} />
+      <div className="flex-1 min-w-0">
+        <div className="focus:outline-none">
+          <div className="flex items-center justify-between mb-1">
+            <p className="text-sm font-medium text-gray-900">{data.name}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserBox;
